66020a086cce0328c7cba429de43a2140fc759c4
John Sturgeon <john.sturgeon@me.com>
2020-11-18 19:22:03 -0800

    Removing shell scripts, adding testing, and organizing the code
    This addresses issues: #61 and #29  Still a lot of work to do, but it's a start

--- bin/healthchecks/bot_check.py
+++ bin/healthchecks/bot_check.py
@@ -1,26 +1,20 @@
-#!../env/bin/python
-""" Discord bot runs in the background and handles all requests to discord """
+#!/usr/bin/python3
+"""Discord bot runs in the background and handles all requests to discord."""
 import os
 import urllib.request
 import socket
-import json
-import sentry_sdk
-from sentry_sdk import add_breadcrumb, capture_exception, capture_message
+from init import get_settings
 
-dirname = os.path.dirname(os.path.abspath(__file__))
-conf_path = os.path.normpath(os.path.join(dirname, '../conf/settings.json'))
-with open(conf_path) as config_file:
-    settings = json.load(config_file)
+settings = get_settings()
+
+# pylint: disable=wrong-import-position
+import sentry_sdk  # noqa: E402
+from sentry_sdk import capture_exception, capture_message  # noqa: E402
 
 SENTRY_DSN = settings['config']['sentry_dsn']
 ENVIRONMENT = settings['config']['environment']
 BOT_CHECK_URL = settings['healthchecks']['discord_bot_check_url']
 sentry_sdk.init(dsn=SENTRY_DSN, environment=ENVIRONMENT)
-add_breadcrumb(
-    category='config',
-    message=f'Configuration file: {conf_path}',
-    level='debug',
-)
 stream = os.popen('pgrep -f discord_bot_service.py', mode='r')
 pid = stream.read()
 pid_str = pid.rstrip()
---
+++ bin/healthchecks/web_site_check.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Discord bot runs in the background and handles all requests to discord."""
+import urllib.request
+import socket
+from init import get_settings
+
+settings = get_settings()
+
+# pylint: disable=wrong-import-position
+import sentry_sdk  # noqa: E402
+from sentry_sdk import capture_exception, capture_message  # noqa: E402
+SENTRY_DSN = settings['config']['sentry_dsn']
+ENVIRONMENT = settings['config']['environment']
+FLASK_SITE_URL = settings['config']['login_url']
+WEB_CHECK_URL = settings['healthchecks']['tgfp_web_check_url']
+sentry_sdk.init(dsn=SENTRY_DSN, environment=ENVIRONMENT)
+
+try:
+    retcode = urllib.request.urlopen(FLASK_SITE_URL).getcode()
+except socket.error:
+    retcode = 404
+
+if retcode == 200:
+    try:
+        urllib.request.urlopen(WEB_CHECK_URL, timeout=10)
+    except socket.error as exception:
+        capture_exception(exception)
+else:
+    capture_message("TGFP Web Site is DOWN")
