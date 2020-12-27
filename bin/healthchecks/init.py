2d0e2436d2a5a3e4cbca2afd99ecff2c5cf0c9e9
John Sturgeon <john.sturgeon@me.com>
2020-12-27 09:50:01 -0800

    Fixing all PyCharm 'inspection' issues
    * Moved common code from 'init' to a single file

--- bin/healthchecks/init.py
+++ bin/healthchecks/init.py
@@ -1,40 +1,8 @@
 """
-Drop this in any new folder to get the interpreter relative to the folder's location.
-
-call settings = get_settings(), this will load the interpreter for your file
-and return the settings
+Load the interpreter and settings relative to this folder
 """
-import os
-import sys
-import json
-
-RELATIVE_INTERP_LOC = "../../env/bin/python"
-RELATIVE_CONF_JSON = "../../conf/settings.json"
-
-
-def reload_interp():
-    """
-    Reload the interpreter to the virtual environment's version.
-
-    Return:
-        returns the interpreter for the 'healthchecks' folder.
-    """
-    dirname = os.path.dirname(os.path.abspath(__file__))
-    new_interp = os.path.normpath(os.path.join(dirname, RELATIVE_INTERP_LOC))
-    if sys.executable != new_interp:
-        os.execl(new_interp, new_interp, *sys.argv)
+import common_init
 
 
 def get_settings():
-    """
-    Get the settings dictionary for the current environment.
-
-    Return:
-        returns a dictionary of settings.
-    """
-    reload_interp()
-    dirname = os.path.dirname(os.path.abspath(__file__))
-    conf_path = os.path.normpath(os.path.join(dirname, RELATIVE_CONF_JSON))
-    with open(conf_path) as config_file:
-        app_settings = json.load(config_file)
-    return app_settings
+    return common_init.get_settings("../../", __file__)
