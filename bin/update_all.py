89205e74959827b0006a9def2343949438b5481c
John Sturgeon <john.sturgeon@me.com>
2021-09-04 20:39:31 -0700

    pylint

--- bin/update_all.py
+++ bin/update_all.py
@@ -1,7 +1,7 @@
 """This is a 'do everything' script for updating the scores in mongo."""
 import os
 
-from config import get_config
+from instance.config import get_config
 from update_scores import main as update_scores
 from mongo_backup import main as backup_db
 from update_win_loss import main as update_win_loss
