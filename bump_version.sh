873e698b0c201daa2a63a455f0f47132f6b700f1
John Sturgeon <john.sturgeon@me.com>
2022-08-30 16:40:35 -0400

    fixed the bump version script to push to main

--- bump_version.sh
+++ bump_version.sh
@@ -2,7 +2,8 @@
 # usage: [major, minor, patch]
 poetry version $1
 git add .
-git commit -m "bumped version"
+git commit -m "$2"
+git push
 NEW_VERSION=`poetry version -s`
 git tag v${NEW_VERSION}
 git push origin v${NEW_VERSION}
